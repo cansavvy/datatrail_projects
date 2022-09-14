c7dad8e636fe238221a2e89159725d8da07ec5ac
cansavvy <cansav09@gmail.com>
2022-09-14 20:26:56 +0000

    add all the files

--- 01_Forming_Questions/util/set-up.R
+++ 01_Forming_Questions/util/set-up.R
@@ -1,24 +1,22 @@
 # C. Savonen 
 # 2022
 
-# Set up the dataset for the first_project.Rmd
+# Set up the dataset for the countries_project.Rmd
+
+library(magrittr)
 
 # Find .git root directory
 root_dir <- rprojroot::find_root(rprojroot::has_dir(".git"))
 
-candy_df <- read_csv(file.path(root_dir, "01_Forming_Questions", "data", "raw", "candy-data.csv"), 
+
+# Neaten up countries df
+countries_df <- read_csv(file.path(root_dir, "03_Cleaning_the_Data", "data", "raw", "countries_of_the_world.csv"), 
                      show_col_types = FALSE)
+  
+countries_df <- countries_df %>% 
+  rename_all(~str_remove_all(., "\\(|\\)|\\.|\\$")) %>% 
+  rename_all(~str_replace_all(., " ", "_")) %>% 
+  rename_all(~str_replace_all(., "\\/", "_to_")) %>% 
+  rename(GDP = GDP__per_capita)
 
-candy_df <- candy_df %>% 
-  dplyr::mutate_at(
-    dplyr::vars(chocolate, 
-                fruity, 
-                caramel, 
-                peanutyalmondy, 
-                nougat, 
-                crispedricewafer, 
-                hard, 
-                bar, 
-                pluribus), as.logical)
-
-write_csv(candy_df, file.path(root_dir, "01_Forming_Questions", "data", "candy-data.csv"))
+write_csv(countries_df, file.path(root_dir, "03_Cleaning_the_Data", "data", "countries_of_the_world.csv"))
